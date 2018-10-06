051c3b80b8d2d11c3d03d287af760458e20917ed
Alejandro Capparelli <alejandro.capparelli@gmail.com>
2018-10-06 19:29:22 +0200

    Set Local storage with login and loggout status

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import React, { Component } from 'react';
 import { createStore } from 'redux';
 import styled from 'styled-components';
 
+
+//Reducers 
 const rootReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'LOGIN':
     return {logged: state.logged = true }
+    case 'LOGGOUT':
+    return {logged: state.logged = false }
   }
   return state
 }
@@ -15,14 +19,20 @@ const store = createStore(rootReducer)
 
 store.subscribe (() => {
   console.log('store updated', store.getState())
+  localStorage.setItem('logged', store.getState().logged )
 })
 
 const initialState = {
   logged: false
 }
 
+//Actions
 const userLogin = () => store.dispatch({
   type: 'LOGIN'
+})
+
+const userLoggout = () => store.dispatch({
+  type: 'LOGGOUT'
 })
 
 class LoginPage extends React.Component {
@@ -59,7 +69,7 @@ class LoginPage extends React.Component {
 
   submit = (e) => {
     e.preventDefault()
-    this.validate() ? userLogin() : console.log('datos incorrectos')
+    this.validate() ? userLogin() : document.querySelector('.error-message').innerHTML = 'Datos incorrectos'
   }
 
   changeName = (e) => {
@@ -72,6 +82,7 @@ class LoginPage extends React.Component {
 
   render() {
     return (
+      <React.Fragment>
       <LoginForm>
         <form onSubmit={this.submit} >
           <label>Nombre: </label> 
@@ -80,7 +91,10 @@ class LoginPage extends React.Component {
             <input type="email" onChange={this.changeEmail}  ></input>
           <button type="submit" >Login</button>
         </form>
+        <div className="error-message"></div>
       </LoginForm>
+      <button onClick={userLoggout} >loggout</button>
+      </React.Fragment>
     );
   }
 }
