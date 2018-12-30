3c1fc7aa3fc845891720924f715d9cc3ecd5546c
Alejandro Capparelli <alejandro.capparelli@gmail.com>
2018-12-30 13:15:57 +0100

    Add reject friendship functionality

--- src/components/OwnProfilePage.js
+++ src/components/OwnProfilePage.js
@@ -57,31 +57,50 @@ const FormMessages = ({ ...props }) => {
     )
 }
 
-const postNotificationFriendshipApproved = (request) => {
+const postNotificationFriendshipApproved = (request, approve) => {
     const requestsContainer = document.querySelector(`[data-name=${request}]`)
-    requestsContainer.innerHTML = '¡Has aceptado la solicitud!'
-    const button = document.querySelector(`[data-button=${request}]`)
-    button.classList.add('hidden')
+    const button = document.querySelectorAll(`[data-button=${request}]`)
+    button.forEach(btn => {
+        btn.classList.add('hidden')
+    }) 
+    if(approve){
+        requestsContainer.innerHTML = '¡Has aceptado la solicitud!'
+    } else {
+        requestsContainer.innerHTML = 'Has rechazado la solicitud... :('
+    }
 }
 
-const getApproveRequestData = (request, author) => {
-    const userApproving = sessionStorage.getItem('userID')
-    const userAccepted = request
+const getApproveRequestData = (request, author, approve ) => {
+
+    //Get all requests from localStorage into an Array
+    const allRequests = JSON.parse(localStorage.getItem(author + ' requested by ' ))
+
+    //find the index of the user requesting friendship
+    const index = allRequests.findIndex( item => item === request )
+
+    //Eliminate only the user approved/rejected from the array
+    allRequests.splice(index, 1)
+
+    //Set a new Array in localStorage
+    localStorage.setItem( author + ' requested by ', JSON.stringify(allRequests) )
     
-    localStorage.setItem(userAccepted + ' accepted by ' + userApproving, true)
-    postNotificationFriendshipApproved(request)
-    localStorage.removeItem(author + ' requested by ')
-    
+    if (approve) {
+        localStorage.setItem( request + ' accepted by ' + author, true )
+        postNotificationFriendshipApproved(request, approve)
+    } else {
+        postNotificationFriendshipApproved(request)
+    }
 }
 
 const ViewRequests = ({requests, author}) => 
 requests&&
     requests.map(request => {
         return(
-            <div className="requests-container" >
+            <div key={request} className="requests-container" >
                 <h2>Hola {sessionStorage.getItem('activeUser')} </h2>
-                <p className="requestNotification" data-name={request} >tienes una solicitud de amistad de: <span>{request} </span> </p>
-                <button className="request-button" data-button={request} onClick={() => getApproveRequestData(request, author)} >Aprobar</button>
+                <p className="requestNotification" data-name={request} >Tienes una solicitud de amistad de: <span>{request} </span> </p>
+                <button key="accept-button" data-button={request} onClick={() => getApproveRequestData(request, author, 'approve')} >Aprobar</button>
+                <button key="reject-button" data-button={request} onClick={() => getApproveRequestData(request, author)}>Rechazar</button>
             </div>
         )
      }) || null
